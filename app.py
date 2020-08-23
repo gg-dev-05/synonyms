ccb34e801093d0e52e059b5a8d6df2338110becd
gg-dev-05 <garvitgalgat@gmail.com>
2020-08-23 16:56:04 +0530

    for heroku

--- app.py
+++ app.py
@@ -28,12 +28,12 @@ def index():
 
     print(word)
     chrome_options = webdriver.ChromeOptions()
-    #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),options=chrome_options)
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),options=chrome_options)
+    # driver = webdriver.Chrome(options=chrome_options)
     # url = "https://www.thesaurus.com/browse/{}?s=t".format(word)
     url = 'https://www.wordsapi.com/#try'
 # ----------------------------------------------------------------------------------------------------------------------------
