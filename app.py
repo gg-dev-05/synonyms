6d1fa821b8775bfde1c95a6ae2cf9fa852961bb9
gg-dev-05 <garvitgalgat@gmail.com>
2020-08-25 14:06:46 +0530

    optimized for single user, multiple users disabled

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -12,39 +12,55 @@ import time
 app = Flask(__name__)
 
 
+url = 'https://www.wordsapi.com/#try'
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),options=chrome_options)
+# driver = webdriver.Chrome(options=chrome_options)
+print("driver get")
+driver.get(url)
+
+def init():
+    global url
+    print("in init")
+    global driver, chrome_options
+    # driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),options=chrome_options)
+    driver.get(url)
+
+print("loaded")
 @app.route('/')
 def home():
-
+    print(" in home")
     return "connected"
 
 
 @app.route('/app', methods=['POST'])
 def index():
 
+    print("in app")
+    global driver
     req = request.get_json()
     word = ""
     word = req['word']
 
     print(word)
-    chrome_options = webdriver.ChromeOptions()
-    #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),options=chrome_options)
-    # driver = webdriver.Chrome(options=chrome_options)
-    # url = "https://www.thesaurus.com/browse/{}?s=t".format(word)
-    url = 'https://www.wordsapi.com/#try'
-# ----------------------------------------------------------------------------------------------------------------------------
-# ----------------------------------------------------------------------------------------------------------------------
-    
 
+    try:
+        print(driver.title)
+        driver.refresh()
+    except WebDriverException:
+        init()
+    
     print("url opened")
     try:
         print("get url")
         print(url)
-        driver.get(url)
+        
         xpath_input = "//*[@id=\"word\"]"
         xpath_syn = "/html/body/div[1]/div[3]/div/form/div/div[1]/div[2]/select/option[3]"
         xpath_button = "//*[@id=\"getWord\"]"
@@ -82,35 +98,9 @@ def index():
         if(len(output) == 0):
             output = "No Results"
 
-        driver.close()
+
         return output
-        # # xpath_for_synonyms = '//*[@id="root"]/div/div/div[2]/main/section/section/div[2]/ul'
-        # # xpath_for_suggestions = '//*[@id="root"]/div/div/div[2]/main/section/section/div[2]/div/h2[2]'
-        # print("find elements by synonyms")
-        # items = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, xpath_for_synonyms)))
-        # print("find elements by suggestions")
-        # chk = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, xpath_for_suggestions)))
-
-        # print("check value of chk")
-        # if(len(chk) > 0):
-        #     print("Suggested Alternatives")
-        #     output += "Suggested Alternatives\n"
         
-        # else:
-        #     print("Synonyms")
-        #     output += "Synonyms\n"
-
-        # print("add all")
-        # sym = items.text
-        # lis = sym.split("\n")
-        # for i in range(len(lis)):
-        #     print(lis[i])
-        #     output += lis[i]
-        #     output += "\n"
-        
-        
-        # driver.close()
-        # return output
 
     except Exception as e:
         driver.close()
@@ -119,5 +109,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(threaded=True)
+    #app.debug = True
+    app.run()
